760e9a0c8bafaf7575d24b5f8a0ef285963cfe7e
JCoji <jcoji022@uottawa.ca>
2026-04-04 04:51:29 -0400

    Changed skybox to panorama from https://screamingbrainstudios.itch.io/cloudy-skyboxes-pack

--- src/skybox.ts
+++ src/skybox.ts
@@ -1,7 +1,7 @@
 import * as THREE from 'three';
 
 export const addSkybox = (scene: THREE.Scene): void => {
-    const skyboxTexture = new THREE.TextureLoader().load('/Skybox-512x512.png');
+    const skyboxTexture = new THREE.TextureLoader().load('/Panorama_Sky_06.png');
     const skybox = new THREE.Mesh(
         new THREE.SphereGeometry(500, 60, 40),
         new THREE.MeshBasicMaterial({ map: skyboxTexture, side: THREE.BackSide })
